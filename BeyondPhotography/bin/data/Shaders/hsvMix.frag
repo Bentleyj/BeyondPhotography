8da6a086e282e2255f4fd31c6a88d98ce203af2d
Bentleyj <jgbentley10@gmail.com>
2018-06-13 18:33:04 +0100

    Rename sinCity Red Effect and add hsv mix effect

---
+++ BeyondPhotography/bin/data/Shaders/hsvMix.frag
@@ -0,0 +1,33 @@
+#version 120
+
+uniform sampler2DRect inputTexture;
+uniform vec2 resolution;
+
+vec3 rgb2hsv(vec3 c) {
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+vec3 hsv2rgb(vec3 c) {
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+void main() {
+    
+    vec2 uv = gl_FragCoord.xy / resolution;
+    
+    uv.y = 1.0 - uv.y;
+    
+    vec2 uvAbs = uv * resolution;
+    
+    vec3 tc = texture2DRect(inputTexture, uvAbs).rgb;
+    tc = rgb2hsv(tc);
+//    tc = hsv2rgb(tc);
+
+    gl_FragColor = vec4(tc, 1.0);
+}
--- BeyondPhotography/src/ofApp.cpp
+++ BeyondPhotography/src/ofApp.cpp
@@ -647,13 +647,21 @@ void ofApp::setup(){
     sinCity.height = ofGetHeight();
     effects.push_back(sinCity);
     
-    Effect sinCityRed;
-    sinCityRed.loadShader("Shaders/sinCityRed");
-    sinCityRed.addUniform("resolution", &screenResolution);
-    sinCityRed.addUniform("inputTexture", &images[0]);
-    sinCityRed.width = ofGetWidth();
-    sinCityRed.height = ofGetHeight();
-    effects.push_back(sinCityRed);
+    Effect rThresh;
+    rThresh.loadShader("Shaders/rThresh");
+    rThresh.addUniform("resolution", &screenResolution);
+    rThresh.addUniform("inputTexture", &images[0]);
+    rThresh.width = ofGetWidth();
+    rThresh.height = ofGetHeight();
+    effects.push_back(rThresh);
+    
+    Effect hsvMix;
+    hsvMix.loadShader("Shaders/hsvMix");
+    hsvMix.addUniform("resolution", &screenResolution);
+    hsvMix.addUniform("inputTexture", &images[0]);
+    hsvMix.width = ofGetWidth();
+    hsvMix.height = ofGetHeight();
+    effects.push_back(hsvMix);
     
     string settingsPath = "settings/settings.xml";
     gui.setup("Effects", settingsPath);
